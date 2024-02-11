3b9185a3c1c1523e7efbb01d9890e59e0cb960e6
Wojtek <wojciech.pastor@student.wat.edu.pl>
2024-02-11 21:50:01 +0100

    data base

--- app.py
+++ app.py
@@ -1,29 +1,70 @@
-from flask import Flask, render_template, request, redirect, url_for, session, Response, send_file
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
+from flask import Flask, render_template, request, redirect, url_for, session
 from datetime import datetime
 from io import BytesIO
 import wfdb
+from sqlalchemy import PickleType
 from wfdb import processing
 import matplotlib.pyplot as plt
 import base64
 import numpy
 from scipy.signal import savgol_filter
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 app.secret_key = "klucz"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///measurements.db'
+db = SQLAlchemy(app)
+
+
+class Measurement(db.Model):
+    file = db.Column(db.String, primary_key=True)
+
+
+#
+# with app.app_context():
+#     db.create_all()
+
+
+def load_measurements(session):
+    excluded_numbers = [110, 120]
+
+    for measurement_number in range(100, 125):
+        if measurement_number not in excluded_numbers:
+            file_list = str(measurement_number)
+
+            new_measurement = Measurement(file=file_list)
+            session.add(new_measurement)
+
+    session.commit()
+
+
+# with app.app_context():
+#     load_measurements(db.session)
+
+
+def get_measurement_file(measurementa):
+    measurement_file = Measurement.query.filter_by(file=measurementa).first()
+    if measurement_file:
+        return measurement_file.file
+    else:
+        return None
+
 
 # defincja zmiennych globalnych
 przesuniecie = 0
 fs = 360
 nazwa_pliku = '100'
-record_all, fields = wfdb.rdsamp(nazwa_pliku, sampfrom=0)
+
+with app.app_context():
+    plik = f"measurements/{get_measurement_file(nazwa_pliku)}"
+
+record_all, fields = wfdb.rdsamp(plik, sampfrom=0)
 qrs_inds_all = wfdb.processing.xqrs_detect(sig=record_all[:, 0], fs=fields['fs'])
-rr_all = wfdb.processing.ann2rr(nazwa_pliku, 'atr', as_array=True)
+rr_all = wfdb.processing.ann2rr(plik, 'atr', as_array=True)
 mean_hr_all = wfdb.processing.calc_mean_hr(rr_all, fs, rr_units='samples')
 heart_rate_all = wfdb.processing.compute_hr(len(record_all), qrs_inds_all, fs)
-annotacje_all = wfdb.rdann(nazwa_pliku, 'atr', sampfrom=0, sampto=len(record_all))
-
+annotacje_all = wfdb.rdann(plik, 'atr', sampfrom=0, sampto=len(record_all))
 
 
 @app.route('/', methods=["POST", "GET"])
@@ -67,7 +108,6 @@ def dashboard():
         return redirect(url_for('login'))
 
 
-
 @app.route('/konfiguracja')
 def konfiguracja():
     if "user" in session:
@@ -88,7 +128,7 @@ def analiza():
         user = session["user"]
 
         global record_all, qrs_inds_all, rr_all, mean_hr_all, heart_rate_all, annotacje_all, przesuniecie
-        
+
         # Reakcja na przyciski przesuniecia
         if 'przesuniecie' in request.args:
             przesuniecie += int(request.args['przesuniecie'])
@@ -97,41 +137,28 @@ def analiza():
         sampto = przesuniecie + 3000
 
         # pobranie rekordów z pliku
-        record, fields = wfdb.rdsamp(nazwa_pliku, sampfrom=sampfrom, sampto=sampto)
-        
-        # zamiana sampli na czas
-        # jeszcze nie używamy czasu na razie pracujemy na samplach
-        czas_start = sampfrom / fs
-        czas_stop = sampto / fs
+        record, fields = wfdb.rdsamp(plik, sampfrom=sampfrom, sampto=sampto)
+
         # zdefiniowanie osi x dla wykresu
         os_x = range(sampfrom, sampto)
 
         # obliczenia qrs, rr, heart rate oraz pobranie annotacji
         qrs_inds = wfdb.processing.xqrs_detect(sig=record[:, 0], fs=fields['fs'])
-        rr = wfdb.processing.ann2rr(nazwa_pliku, 'atr', as_array=True)
+        rr = wfdb.processing.ann2rr(plik, 'atr', as_array=True)
         mean_hr = wfdb.processing.calc_mean_hr(rr, fs, rr_units='samples')
         heart_rate = wfdb.processing.compute_hr(len(record), qrs_inds, fs)
-        annotacje = wfdb.rdann(nazwa_pliku, 'atr', sampfrom=sampfrom, sampto=sampto)
+        annotacje = wfdb.rdann(plik, 'atr', sampfrom=sampfrom, sampto=sampto)
 
-        # filtrowanie EKG
-        smoothed_signal = savgol_filter(record[:, 0], window_length=51, polyorder=3)
-        
-        # wykres z zaznaczonymi qrs
-        # fig=wfdb.plot_items(signal=record, ann_samp=[qrs_inds],return_fig=True)
-        # fig=wfdb.plot_wfdb(record=record, annotation=annotacje,plot_sym=True,title='MIT-BIH Record 100',figsize=(20,6),return_fig=True)
-        # fig.savefig("ann.png")
 
         # przekształcenia
         float_mean_hr = float(mean_hr)
         count_N = 0
         count_A = 0
-
+        count_slash = 0
+        count_V = 0
+        count_L = 0
         float_mean_hr_all = float(mean_hr_all)
-        count_N_all = 0
-        count_A_all = 0
-
         float_hr = []
-
         float_hr_all = []
 
         for i in heart_rate:
@@ -139,7 +166,7 @@ def analiza():
                 float_hr.append(float_mean_hr)
             else:
                 float_hr.append(float(i))
-        
+
         for i in heart_rate_all:
             if numpy.isnan(i):
                 float_hr_all.append(float_mean_hr_all)
@@ -150,7 +177,7 @@ def analiza():
 
         for i in annotacje.symbol:
             char_symbol.append(str(i))
-        
+
         char_symbol_all = []
 
         for i in annotacje_all.symbol:
@@ -170,8 +197,14 @@ def analiza():
             f.write(f"Liczba annotacji A: {char_symbol.count('A')}\n")
             count_N = char_symbol.count('N')
             count_A = char_symbol.count('A')
+            count_slash = char_symbol.count('/')
+            count_V = char_symbol.count('V')
+            count_L = char_symbol.count('L')
             session["count_N"] = count_N
             session["count_A"] = count_A
+            session["count_slash"] = count_slash
+            session["count_V"] = count_V
+            session["count_L"] = count_L
             f.write(f"Maksymalny heart rate: {max(float_hr):.2f}\n")
             f.write(f"Minimalny heart rate: {min(float_hr):.2f}\n")
             for i, (symbol, sample, hr) in enumerate(zip(annotacje.symbol, annotacje.sample, float_hr), start=1):
@@ -185,6 +218,9 @@ def analiza():
 
         count_N_all = char_symbol_all.count('N')
         count_A_all = char_symbol_all.count('A')
+        count_slash_all = char_symbol_all.count('/')
+        count_V_all = char_symbol_all.count('V')
+        count_L_all = char_symbol_all.count('L')
 
         # Średni HR, Max HR, Min HR
         avg_hr = f"{mean_hr:.2f}"
@@ -197,50 +233,29 @@ def analiza():
         max_hr_all = f"{max(float_hr_all):.2f}"
         min_hr_all = f"{min(float_hr_all):.2f}"
 
-        # Plot signal
-        # plt.figure(figsize=(20, 6))
-        # plt.style.use('ggplot')
-        # plt.plot(record[:, 1], label='Channel 1')
-        # plt.title('MIT-BIH Record 100')
-        # plt.xlabel('Time (samples)')
-        # plt.ylabel('ECG Signal')
-
-        # Save plot to BytesIO buffer
         buf = BytesIO()
         plt.figure(figsize=(12, 6))
-        # plotuje samo EKG
         plt.plot(os_x, record[:, 0])
-        # plotuje EKG oraz to pomarańczowe coś
-        # plt.plot(os_x, record) 
-        # plotuje zfiltrowane EKG
-        # plt.plot(os_x, smoothed_signal, color='blue')
         plt.xlabel('Sample')
         plt.ylabel('Amplitude')
 
         # Dodaj zaznaczenia annotacji
         for sample, symbol in zip(annotacje.sample, annotacje.symbol):
             if sampfrom <= sample < sampto and sample < len(record_all):
-                # annotacje dla normalnego EKG
                 plt.text(sample, record_all[sample, 0], symbol, fontsize=10, color='red')
-                # annotacje dla zfiltrowanego EKG
-                # plt.text(sample, smoothed_signal[sample - sampfrom], symbol, fontsize=10, color='red')
 
         plt.grid(True)
         plt.tight_layout()
-        # plt.show()
-        # buf1 = BytesIO()
         plt.savefig(buf, format='png')
-        # fig.savefig(buf1, format='png')
         buf.seek(0)
         plt.close()
 
-        # Convert image to base64
         chart_data = base64.b64encode(buf.getvalue()).decode('utf-8')
-        # chart_data1 = base64.b64encode(buf1.read()).decode('utf-8')
-        # zwrócenie wszystkich danych do html
         return render_template('analiza.html', chart_data=chart_data, avg_hr=avg_hr, max_hr=max_hr, min_hr=min_hr,
-                               total_ann=total_ann, count_N=count_N, count_A=count_A, avg_hr_all=avg_hr_all, max_hr_all=max_hr_all, min_hr_all=min_hr_all,
-                               total_ann_all=total_ann_all, count_N_all=count_N_all, count_A_all=count_A_all, content=user)
+                               total_ann=total_ann, count_N=count_N, count_A=count_A, count_slash=count_slash, count_V=count_V,
+                               count_L=count_L, avg_hr_all=avg_hr_all, max_hr_all=max_hr_all, min_hr_all=min_hr_all,
+                               total_ann_all=total_ann_all, count_N_all=count_N_all, count_A_all=count_A_all, count_slash_all=count_slash_all,
+                               count_V_all=count_V_all, count_L_all=count_L_all, content=user)
     else:
         return redirect(url_for('login'))
 
